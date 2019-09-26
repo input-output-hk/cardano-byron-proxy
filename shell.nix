fd5141fea5eaf3759b61eb02000d7f094cc51af8
Kosyrev Serge <serge.kosyrev@iohk.io>
2019-09-26 19:15:35 +0300

    shell.nix:  add

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ withHoogle ? true
+}:
+let
+  default = import ./default.nix {};
+in
+default.nix-tools._raw.shellFor {
+  packages    = ps: with ps; [ cardano-byron-proxy ];
+  withHoogle  = withHoogle;
+  buildInputs =
+  (with default.nix-tools._raw; [
+    cabal-install.components.exes.cabal
+    ghcid.components.exes.ghcid
+  ]) ++
+  (with default.nix-tools._raw._config._module.args.pkgs; [
+    tmux
+  ]);
+}
