a6dc02fc28f74e8d220ac4256bdbd1aaa06f8692
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-09-30 19:00:23 +1000

    Nix voodoo

--- nix/.stack.nix/ouroboros-network.nix
+++ nix/.stack.nix/ouroboros-network.nix
@@ -23,6 +23,7 @@
           (hsPkgs.typed-protocols-cbor)
           (hsPkgs.io-sim-classes)
           (hsPkgs.contra-tracer)
+          (hsPkgs.cardano-prelude)
           (hsPkgs.async)
           (hsPkgs.binary)
           (hsPkgs.bytestring)
@@ -71,6 +72,7 @@
             (hsPkgs.binary)
             (hsPkgs.bytestring)
             (hsPkgs.cardano-binary)
+            (hsPkgs.cardano-prelude)
             (hsPkgs.cborg)
             (hsPkgs.containers)
             (hsPkgs.contra-tracer)
@@ -105,6 +107,7 @@
             (hsPkgs.base)
             (hsPkgs.bytestring)
             (hsPkgs.cardano-binary)
+            (hsPkgs.cardano-prelude)
             (hsPkgs.cborg)
             (hsPkgs.containers)
             (hsPkgs.contra-tracer)
@@ -129,8 +132,8 @@
     } // {
     src = (pkgs.lib).mkDefault (pkgs.fetchgit {
       url = "https://github.com/input-output-hk/ouroboros-network";
-      rev = "a492a9897e38db07aea440d79a63c6a5a3171906";
-      sha256 = "0ib9lb5jn8v72nynsp2iigmialv6cyfw3zsz9rqrpwrjm1fliyxc";
+      rev = "e7a13e34d45f17e4d03a2c96a9c3469e0c6f2739";
+      sha256 = "0m80nqyg5rf7y8q7dnvdlpgs0k67dfjgi9kwqxidkr5k0f6jr50n";
       });
     postUnpack = "sourceRoot+=/ouroboros-network; echo source root reset to \$sourceRoot";
     }
