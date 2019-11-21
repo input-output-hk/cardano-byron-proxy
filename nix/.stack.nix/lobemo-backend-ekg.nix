ba95aa48d9b15d8d4638ed6dfc070adec800fb51
Alexander Diemand <codieplusplus@apax.net>
2019-11-21 15:18:29 +0100

    nixified
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

---
+++ nix/.stack.nix/lobemo-backend-ekg.nix
@@ -0,0 +1,44 @@
+{ system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.0";
+      identifier = { name = "lobemo-backend-ekg"; version = "0.1.0.0"; };
+      license = "Apache-2.0";
+      copyright = "2019 IOHK";
+      maintainer = "operations@iohk.io";
+      author = "Alexander Diemand";
+      homepage = "https://github.com/input-output-hk/iohk-monitoring-framework";
+      url = "";
+      synopsis = "provides a backend implementation to EKG";
+      description = "";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs.base)
+          (hsPkgs.iohk-monitoring)
+          (hsPkgs.aeson)
+          (hsPkgs.async)
+          (hsPkgs.ekg)
+          (hsPkgs.ekg-core)
+          (hsPkgs.ekg-prometheus-adapter)
+          (hsPkgs.prometheus)
+          (hsPkgs.safe-exceptions)
+          (hsPkgs.stm)
+          (hsPkgs.text)
+          (hsPkgs.time)
+          (hsPkgs.unordered-containers)
+          (hsPkgs.warp)
+          ];
+        };
+      };
+    } // {
+    src = (pkgs.lib).mkDefault (pkgs.fetchgit {
+      url = "https://github.com/input-output-hk/iohk-monitoring-framework";
+      rev = "4956b32f039579a0e7e4fd10793f65b4c77d9044";
+      sha256 = "03lyb2m4i6p7rpjqarnhsx21nx48fwk6rzsrx15k6274a4bv0pix";
+      });
+    postUnpack = "sourceRoot+=/plugins/backend-ekg; echo source root reset to \$sourceRoot";
+    }
